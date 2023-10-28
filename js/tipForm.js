03cc82e34bb1930af013c96bc1d9d1431b74e43a
DlayDlay <dylan.baltrus@gmail.com>
2023-10-28 16:24:43 -0500

    added basic function for bill total input change, math for grand total

--- js/tipForm.js
+++ js/tipForm.js
@@ -6,14 +6,21 @@ document.addEventListener('DOMContentLoaded', function(){
   let tipAmount = document.getElementById("tipAmount");
   let grandTotal = document.getElementById("grandTotal");
   
-  tipSlider.addEventListener("input", tipEdit); 
+  tipSlider.addEventListener("input", tipEdit);
+  billTotal.addEventListener("input", billEdit);  
   
   
   function tipEdit() {
     tipPercent.value = tipSlider.value;
   }
   
-  
+  function billEdit() {
+	let percent = (parseFloat(tipSlider.value) / 100);
+    let subtotal = parseFloat(billTotal.value);
+    let grand = subtotal + (subtotal * percent);
+	tipAmount.value = subtotal * percent;
+    grandTotal.value = grand;
+  }
   
   
   
